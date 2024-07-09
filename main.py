9149d71275f0cbea01215e5b5ea57e2495f2c17c
TheRad1 <landon.bakken@gmail.com>
2024-07-09 12:23:04 -0500

    going to start doing a FLIP type simulation

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def printGrid(screen = None):
                 rowString += f" {grid[column][row]:.3f}"
             print(rowString)
     else:
-        #clear
-        screen.fill((255, 255, 255))
+        recalculateColors()
 
         #print each point
         for row in range(rows):
@@ -28,8 +27,18 @@ def clamp(value, minValue, maxValue):
     return max(min(value, maxValue), minValue)
 
 def getColor(value):
-    num = clamp(int(value * 255), 0, 255)
+    percent = value/maxPressure
+    num = int(percent * 255)
     return (num, num, num)
+
+def recalculateColors():
+    global maxPressure, grid
+
+    maxPressure = grid[0][0]
+    for column in range(columns):
+            for row in range(rows):
+                if grid[column][row] > maxPressure:
+                    maxPressure = grid[column][row]
 
 #creates empty grid
 def createGrid(rows, columns):
@@ -49,22 +58,22 @@ def disperse(multiplier):
             newPressure =  originalPressure - dispersedPressure
             dispersedPressure /= 4
             
-            if column + 1 < len(grid) and grid[column + 1][row] < 1:
+            if column + 1 < len(grid):
                 grid[column + 1][row] += dispersedPressure
             else:
                 newPressure += dispersedPressure
             
-            if column - 1 >= 0 and grid[column - 1][row] < 1:
+            if column - 1 >= 0:
                 grid[column - 1][row] += dispersedPressure
             else:
                 newPressure += dispersedPressure
             
-            if row + 1 < len(grid[0]) and grid[column][row + 1] < 1:
+            if row + 1 < len(grid[0]):
                 grid[column][row + 1] += dispersedPressure
             else:
                 newPressure += dispersedPressure
             
-            if row - 1 >= 0 and grid[column][row - 1] < 1:
+            if row - 1 >= 0:
                 grid[column][row - 1] += dispersedPressure
             else:
                 newPressure += dispersedPressure
@@ -76,11 +85,18 @@ def disperse(multiplier):
 def applyGravity(multiplier):
     for column in range(columns):
         for row in range(rows):
-            if row + 1 < len(grid[0]) and grid[column][row + 1] < 1:
+            if row + 1 < len(grid[0]):
                 dispersedPressure = grid[column][row] * multiplier
                 grid[column][row] -= dispersedPressure
                 grid[column][row + 1] += dispersedPressure
-        
+
+
+def text_to_screen(screen, text, x, y, size=50, color=(200, 000, 000), font_type=pygame.font.get_default_font()):
+	text = str(text)
+	font = pygame.font.Font(font_type, size)
+	text = font.render(text, True, color)
+	screen.blit(text, (x, y))
+
 
 #start display
 pygame.init()
@@ -89,11 +105,12 @@ pygame.display.set_caption("Fluid simulation")
 
 #info
 grid = createGrid(rows, columns)
-gravity = 20
-speed = 3
+gravity = 30
+speed = 30
 dt = 0
-frames = 0
+maxPressure = 0 #not a limit, this is for coloring
 lastFrameTime = time.time()
+font = pygame.font.Font(None, 36)
 
 #loop
 while True:
@@ -102,18 +119,18 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    dt = time.time() - lastFrameTime
-    lastFrameTime = time.time()
-    print(dt)
-
     #grid[int(columns/2)][int(rows/2)] = 1
-    grid[10][int(10)] = 1
+    grid[10][int(10)] += 1
     
     disperse(speed * dt)
     applyGravity(gravity * dt)
 
     printGrid(screen)
-    frames += 1
+
+    dt = time.time() - lastFrameTime
+    lastFrameTime = time.time()
+    fps_text = font.render(f"FPS: {int(1/dt)}", True, (255, 0, 0))
+    screen.blit(fps_text, (10, 10))
     #printGrid()
 
     pygame.display.flip()
